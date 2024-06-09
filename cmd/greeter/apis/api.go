45def4cdaff28c523a523ca46d50ff5e1c02ac9a
aadityasinha-dotcom <aadityasinha009@gmail.com>
2024-06-09 23:13:12 +0530

    api calls

--- cmd/greeter/apis/api.go
+++ cmd/greeter/apis/api.go
@@ -3,9 +3,48 @@ package api
 import (
   "fmt"
   "github.com/spf13/cobra"
+  "bytes"
+  "encoding/json"
+  "net/http"
 )
 
 func WeatherCommand() *cobra.Command {
+
+  data := map[string]interface{}{
+    "parent": map[string]interface{}{
+      "database_id": "YOUR_DATABASE_ID",
+    },
+    "properties": map[string]interface{}{
+      "Name": map[string]string{
+        "title": "Task Name",
+      },
+        // Add other properties as needed
+    },
+  }
+
+    // Convert data to JSON
+    jsonData, err := json.Marshal(data)
+    if err != nil {
+        panic(err)
+    }
+
+  client := &http.Client{}
+    req, err := http.NewRequest("POST", "https://api.notion.com/v1/pages", bytes.NewBuffer(jsonData))
+    if err != nil {
+        panic(err)
+    }
+
+    // Include authentication token
+    req.Header.Set("Authorization", "Bearer YOUR_API_KEY")
+    req.Header.Set("Content-Type", "application/json")
+
+    // Send request
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
   return &cobra.Command{
     Use: "api",
     Short: "API the user",
