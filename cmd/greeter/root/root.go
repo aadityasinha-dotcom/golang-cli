5fe725c34b552088c9db4c9f7dc7e12c27ef7294
aadityasinha-dotcom <aadityasinha009@gmail.com>
2024-06-19 23:05:46 +0530

    changes to typeo

--- cmd/greeter/root/root.go
+++ cmd/greeter/root/root.go
@@ -9,9 +9,9 @@ import (
 
 func RootCommand() *cobra.Command {
   cmd := &cobra.Command{
-    Use: "greeter",
-    Short: "greater is friendly",
-    Long: "greeter is a friendly cli",
+    Use: "Greeter",
+    Short: "Greater is friendly",
+    Long: "Greeter is a friendly cli",
     Run: func(cmd *cobra.Command, args []string) {
       fmt.Println("Welcome to Greeter")
     },
