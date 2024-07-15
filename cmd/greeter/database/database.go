8bf4238bf5bd991b68e120cf7bb2de1b0c618cb3
aadityasinha-dotcom <aadityasinha009@gmail.com>
2024-07-15 22:54:21 +0530

    changes

--- cmd/greeter/database/database.go
+++ cmd/greeter/database/database.go
@@ -39,14 +39,14 @@ func queryDatabase(apiKey string) {
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Notion-Version", notionAPIVersion)
 
-	// // Send the request
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	fmt.Println("Error sending request:", err)
-	// 	return
-	// }
-	// defer resp.Body.Close()
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
 
 	// Read the response body
 	body, err := ioutil.ReadAll(resp.Body)
