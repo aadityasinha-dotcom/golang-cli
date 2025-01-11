e2099fb04fd07c766bedbc762eedf5427b6aa332
aadityasinha-dotcom <aadityasinha009@gmail.com>
2025-01-11 23:51:58 +0530

    api config

--- cmd/greeter/apis/api.go
+++ cmd/greeter/apis/api.go
@@ -15,9 +15,9 @@ func WeatherCommand() *cobra.Command {
 
     client := &http.Client{}
     req, err := http.NewRequest("POST", "https://api.notion.com/v1/pages", bytes.NewBuffer(jsonData))
-    // if err != nil {
-    //     panic(err)
-    // }
+    if err != nil {
+        panic(err)
+    }
 
     req.Header.Set("Authorization", "Bearer YOUR_API_KEY")
     req.Header.Set("Content-Type", "application/json")
